a62c3c03914a4a81e1022d5b7417842d4a7b3496
Yiu Lap Sang <yiulapsang@Yius-MacBook-Air.local>
2020-04-05 01:20:18 +0800

    seperate each request

--- store_app.py
+++ store_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -29,57 +29,79 @@ Store example:
                 .
 }
 """
-data = {}
+data = {"Mary":
+    {
+        "name": "Mary",
+        "items": {
+            "apple": {
+                "name": "apple",
+                "price": 100,
+                "quantity": 1
+            }
+        }
+    }
+}
+
+
+# GET /store -> get storeList
+@app.route('/store', methods=['GET'])
+def read_stores():
+    return jsonify(data)
 
 
 # POST /store data: {name:}  -> create new store with given name
-# GET /store                 -> get storeList
-@app.route('/store', methods=['GET', 'POST'])
-def store_request():
-    if request.method == "GET":
-        return data
-    elif request.method == "POST":
-        store_name = request.get_json(silent=True)['name']
-        if store_name not in data:
-            data[store_name] = {'name': store_name, 'items': {}}
-        else:
-            return {'error': f'store<{store_name}> exist'}
+@app.route('/store', methods=['POST'])
+def add_store():
+    store_name = request.get_json(silent=True)['name']
+    if store_name not in data:
+        data[store_name] = {
+            'name': store_name,
+            'items': {}
+        }
+    else:
+        return jsonify({'message': f'store<{store_name}> exist'})
 
-        return data
+    return jsonify(data)
 
 
 # no space between string:store_name
 # GET /store/<string:store_name>  -> get store resource with given name
 # store_name as arg of method
 @app.route('/store/<string:store_name>', methods=['GET'])
-def the_store_request(store_name):
+def read_store(store_name):
     if request.method == "GET":
         if store_name not in data:
-            return {'error': f'store<{store_name}> does not exist'}
-        return data[store_name]
+            return jsonify({'message': f'store<{store_name}> does not exist'})
+        return jsonify(data[store_name])
 
 
 # POST /store/<string:store_name>/item data:{name:, price}-> create item in the store with given name
+@app.route('/store/<string:store_name>/item', methods=['POST'])
+def add_item(store_name):
+    item = request.get_json(silent=True)
+    item_name = item['name']
+    if store_name in data:
+        items = data[store_name]['items']
+        if item_name in items:
+            return jsonify({'message': f'item<{item_name}> exist'})
+        else:
+            items[item_name] = item
+            return jsonify(data[store_name]['items'])
+    else:
+        return jsonify({'message': f'store<{store_name}> does not exist'})
+
+
 # GET /store/<string:store_name>/item  -> get the item in the store
-@app.route('/store/<string:store_name>/item', methods=['GET', 'POST'])
-def item_request(store_name):
-    if request.method == "GET":
-        if store_name in data:
-            return data[store_name]['items']
+@app.route('/store/<string:store_name>/item/<string:item_name>', methods=['GET'])
+def add_item(store_name, item_name):
+    if store_name in data:
+        items = data[store_name]['items']
+        if item_name in items:
+            return jsonify(items[item_name])
         else:
-            return {'error': f'store<{store_name}> does not exist'}
-    elif request.method == "POST":
-        item = request.get_json(silent=True)
-        item_name = item['name']
-        if store_name in data:
-            items = data[store_name]['items']
-            if item_name in items:
-                return {'error': f'item<{item_name}> exist'}
-            else:
-                items[item_name] = item
-                return data[store_name]['items']
-        else:
-            return {'error': f'store<{store_name}> does not exist'}
+            return jsonify({'message': f'item<{item_name}> does not exist'})
+    else:
+        return jsonify({'message': f'store<{store_name}> does not exist'})
 
 
 if __name__ == '__main__':
